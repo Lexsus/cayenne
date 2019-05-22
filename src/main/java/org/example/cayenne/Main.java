05cc04527f00584cb066edda2bba9f6fc3e95c74
Lexsus <lucassrus@gmail.com>
2019-05-22 18:23:26 +0300

    Init

---
+++ src/main/java/org/example/cayenne/Main.java
@@ -0,0 +1,14 @@
+package org.example.cayenne;
+
+import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.configuration.server.ServerRuntime;
+
+public class Main {
+    public static void main(String[] args) {
+        ServerRuntime cayenneRuntime = ServerRuntime.builder()
+                .addConfig("cayenne-project.xml")
+                .build();
+        ObjectContext context = cayenneRuntime.newContext();
+    }
+
+}
---
+++ src/main/java/org/example/cayenne/persistent/Artist.java
@@ -0,0 +1,9 @@
+package org.example.cayenne.persistent;
+
+import org.example.cayenne.persistent.auto._Artist;
+
+public class Artist extends _Artist {
+
+    private static final long serialVersionUID = 1L; 
+
+}
